f13f562cb9d3ab30aa8d9c3408b0298818419e27
Ha-Lim96 <ouladmansour.adelhalim@gmail.com>
2024-03-17 18:35:15 +0100

    création des entités ##2

--- src/main/java/com/mycompany/gestionstock/gestionDeStock/model/AbstractEntity.java
+++ src/main/java/com/mycompany/gestionstock/gestionDeStock/model/AbstractEntity.java
@@ -5,16 +5,15 @@ import lombok.Data;
 import lombok.Generated;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity implements Serializable {
 
     @Id
---
+++ src/main/java/com/mycompany/gestionstock/gestionDeStock/model/Client.java
@@ -0,0 +1,4 @@
+package com.mycompany.gestionstock.gestionDeStock.model;
+
+public class Client extends AbstractEntity{
+}
